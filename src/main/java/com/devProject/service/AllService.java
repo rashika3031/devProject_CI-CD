e5e1cebe4a75018a18d0b4fc39260e603f2506c2
rashika3031 <rashika.sahu@nagarro.com>
2023-09-28 16:09:03 +0530

    adding devProject files

---
+++ src/main/java/com/devProject/service/AllService.java
@@ -0,0 +1,18 @@
+package com.devProject.service;
+
+public class AllService {
+	
+	public float add(float i,float j) {
+		return i+j;
+	}
+	public float sub(float i,float j) {
+		return i-j;
+	}
+	public float mul(float i,float j) {
+		return i*j;
+	}
+	public float div(float i,float j) {
+		return i/j;
+	}
+
+}
